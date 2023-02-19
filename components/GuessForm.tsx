21dec26b529848ab23f91912eb13283eade75d84
Brian Ho <brianslho@gmail.com>
2023-02-19 04:16:04 -0800

    feat: guess feedback

--- components/GuessForm.tsx
+++ components/GuessForm.tsx
@@ -5,21 +5,23 @@ import {atom, useAtom, useSetAtom} from "jotai";
 import {gameWonAtom, guessedWordsAtom, titleRedactedWords} from "@/pages";
 
 const guessInputAtom = atom<string>("");
+const guessFeedbackAtom = atom<string>("");
 
 export default function GuessForm() {
   const [guessedWords, setGuessedWords] = useAtom(guessedWordsAtom);
   const [guessInput, setGuessInput] = useAtom(guessInputAtom);
+  const [guessFeedback, setGuessFeedback] = useAtom(guessFeedbackAtom);
   const setIsGameWon = useSetAtom(gameWonAtom);
 
   const guessSubmitHandler: FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault(); // Don't reload the page
     const normalizedWord = normalizeWord(guessInput);
     if (normalizedWord.length === 0) {
-      // TODO: "No valid characters were entered. Please use English."
+      setGuessFeedback("No valid characters were entered. Please use English.")
     } else if (guessedWords.has(normalizedWord)){
       // TODO: Scroll to previous guess and highlight it
     } else if (COMMON_WORDS.includes(normalizedWord)){
-      // TODO: "This word is revealed by default if it exists."
+      setGuessFeedback("This word is already revealed by default (if it exists).")
     } else {
       // Valid guess, record it!
       setGuessedWords((prevState) => {
@@ -43,13 +45,14 @@ export default function GuessForm() {
       <label htmlFor="email" className="block font-semibold">
         Guess a word:
       </label>
-      <div className="mt-1 flex gap-x-2">
+      <div className="mt-1 flex gap-x-4">
         <input
           type="text"
           name="word"
           className="block w-full px-4 py-2 border border-gray-800 rounded-md"
           value={guessInput}
           onChange={(event) => setGuessInput(event.target.value)}
+          autoComplete="off"
         />
         <button
           type="submit"
@@ -58,6 +61,7 @@ export default function GuessForm() {
           Guess
         </button>
       </div>
+      <div className="mt-1 italic text-red-500 text-xs">{guessFeedback}</div>
     </form>
   );
 }
