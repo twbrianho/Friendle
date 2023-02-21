a96c38d22312470a77fdfb22714e2826a8afa7e2
Brian Ho <brianslho@gmail.com>
2023-02-20 22:49:43 -0800

    feat: Google Analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,24 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google Tag */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-C4ZSF93086"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-C4ZSF93086');
+          `}
+        </Script>
         <meta
           name="description"
           content="The one where you guess which Friends episode it is by playing a Redactle rip-off."
